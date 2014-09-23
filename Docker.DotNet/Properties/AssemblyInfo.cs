b91855ceddc528573b4075f59daac46b13e30e0d
Josh Garverick <josh.garverick@gmail.com>
2014-09-23 07:19:29 -0400

    Changed AssemblyInfo.cs to remove erroneous Docker.DotNet.PCL namespace.  Also removed sample project.

--- Docker.DotNet/Properties/AssemblyInfo.cs
+++ Docker.DotNet/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Docker.DotNet.PCL")]
+[assembly: AssemblyTitle("Docker.DotNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Docker.DotNet.PCL")]
+[assembly: AssemblyProduct("Docker.DotNet")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
