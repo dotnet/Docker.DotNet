615cf4b96e8293418b00088203a6eb9123461451
Josh Garverick <josh.garverick@gmail.com>
2014-09-24 21:09:20 -0400

    Pre-qualified for testing internals in unit or integration assemblies; created new X509 assembly to isolate the CertificateCredentials goods

--- Docker.DotNet.X509/CertificateCredentials.cs
+++ Docker.DotNet.X509/CertificateCredentials.cs
@@ -1,26 +1,31 @@
-﻿using System.Net.Http;
-using Org.BouncyCastle.X509;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Docker.DotNet
+namespace Docker.DotNet.X509
 {
     public class CertificateCredentials : Credentials
     {
 
-        public X509Certificate ClientCertificate { get; private set; }
+        public X509Certificate2 ClientCertificate { get; private set; }
 
-        public CertificateCredentials(X509Certificate clientCertificate)
+        public CertificateCredentials(X509Certificate2 clientCertificate)
         {
             this.ClientCertificate = clientCertificate;
         }
 
         public override HttpClient BuildHttpClient()
         {
-            PortableWebRequestHandler certHandler = new PortableWebRequestHandler()
+            WebRequestHandler certHandler = new WebRequestHandler()
             {
                 ClientCertificateOptions = ClientCertificateOption.Manual,
                 UseDefaultCredentials = false
             };
-            
+
             certHandler.ClientCertificates.Add(this.ClientCertificate);
 
             return new HttpClient(certHandler);
--- Docker.DotNet.X509/Properties/AssemblyInfo.cs
+++ Docker.DotNet.X509/Properties/AssemblyInfo.cs
@@ -1,22 +1,28 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Docker.DotNet")]
+[assembly: AssemblyTitle("Docker.DotNet.X509")]
 [assembly: InternalsVisibleTo("Docker.DotNet.UnitTests")]
 [assembly: InternalsVisibleTo("Docker.DotNet.IntegrationTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Docker.DotNet")]
+[assembly: AssemblyProduct("Docker.DotNet.X509")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("653dce17-3988-48c4-8f87-6e9968931d9a")]
 
 // Version information for an assembly consists of the following four values:
 //
