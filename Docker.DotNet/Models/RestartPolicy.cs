2cd230eb598cdefe5a59edf595530684dc10f432
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2014-10-22 00:29:13 -0700

    code cosmetics and cleanup after merge

--- Docker.DotNet/Models/RestartPolicy.cs
+++ Docker.DotNet/Models/RestartPolicy.cs
@@ -1,23 +1,20 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 
 namespace Docker.DotNet.Models
 {
     public enum RestartPolicyKind
     {
-        [EnumMember(Value = "always")]
-        Always,
+        [EnumMember(Value = "always")] Always,
 
-        [EnumMember(Value = "on-failure")]
-        OnFailure
+        [EnumMember(Value = "on-failure")] OnFailure
     }
 
-    [DataContract] 
+    [DataContract]
     public class RestartPolicy
     {
-        [DataMember(Name="Name")]
+        [DataMember(Name = "Name")]
         public RestartPolicyKind Name { get; set; }
+
         [DataMember(Name = "MaximumRetryCount")]
         public int MaximumRetryCount { get; set; }
     }
