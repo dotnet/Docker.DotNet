6f256c796bfe30a2915539c229d5e318adf6a34b
Ryan Wentzel <rwentzel@illumina.com>
2015-04-21 20:51:17 -0700

    add IDockerClient interface

---
+++ Docker.DotNet/IDockerClient.cs
@@ -0,0 +1,11 @@
+﻿namespace Docker.DotNet
+{
+    public interface IDockerClient
+    {
+        IImageOperations Images { get; }
+
+        IContainerOperations Containers { get; }
+
+        IMiscellaneousOperations Miscellaneous { get; }
+    }
+}
