b62a218d15c24ef8a9ff03526c0652406a060808
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-09-17 09:16:09 -0700

    Syntax fixes

--- Docker.DotNet/Models/ExecCreateContainerConfig.cs
+++ Docker.DotNet/Models/ExecCreateContainerConfig.cs
@@ -6,16 +6,16 @@ namespace Docker.DotNet.Models
     public class ExecCreateContainerConfig
     {
         [DataMember(Name = "AttachStdin")]
-        public bool ? AttachStdin { get; set; }
+        public bool? AttachStdin { get; set; }
 
         [DataMember(Name = "AttachStdout")]
-        public bool ? AttachStdout { get; set; }
+        public bool? AttachStdout { get; set; }
 
         [DataMember(Name = "AttachStderr")]
-        public bool ? AttachStderr { get; set; }
+        public bool? AttachStderr { get; set; }
 
         [DataMember(Name = "Tty")]
-        public bool ? Tty { get; set; }
+        public bool? Tty { get; set; }
 
         [DataMember(Name = "Cmd")]
         public IList<string> Cmd { get; set; }
