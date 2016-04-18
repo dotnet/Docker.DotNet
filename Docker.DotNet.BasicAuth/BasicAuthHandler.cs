b0183ef8c9dffe5a16eb73d55291aaca958e52bc
Justin Terry <juterry@microsoft.com>
2016-04-18 14:35:26 -0700

    Adding support for building in netstandard1.3 and net46 through .NET CLI
    Signed-off-by: Justin Terry <juterry@microsoft.com>

--- Docker.DotNet.BasicAuth/BasicAuthHandler.cs
+++ Docker.DotNet.BasicAuth/BasicAuthHandler.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Runtime.InteropServices;
-using System.Security;
+
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
+#if !netstandard
+using System.Runtime.InteropServices;
+using System.Security;
+#endif
 
 namespace Docker.DotNet.BasicAuth
 {
     internal class BasicAuthHandler : DelegatingHandler
     {
+#if !netstandard
         private readonly SecureString _username;
         private readonly SecureString _password;
+#else
+        private readonly string _username;
+        private readonly string _password;
+#endif
 
+#if !netstandard
         public BasicAuthHandler(SecureString username, SecureString password, HttpMessageHandler innerHandler) : base(innerHandler)
+#else
+        public BasicAuthHandler(string username, string password, HttpMessageHandler innerHandler) : base(innerHandler)
+#endif
         {
             if (username == null)
             {
@@ -39,12 +52,16 @@ namespace Docker.DotNet.BasicAuth
 
         private string BuildParameters()
         {
-            string authInfo = string.Format("{0}:{1}", ConvertToUnsecureString(_username),
-                ConvertToUnsecureString(_password));
+#if !netstandard
+            string authInfo = string.Format("{0}:{1}", ConvertToUnsecureString(_username), ConvertToUnsecureString(_password));
+#else
+            var authInfo = $"{_username}:{_password}";
+#endif
 
-            return Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            return Convert.ToBase64String(Encoding.GetEncoding(authInfo).GetBytes(authInfo));
         }
 
+#if !netstandard
         private static string ConvertToUnsecureString(SecureString secureString)
         {
             IntPtr unmanagedString = IntPtr.Zero;
@@ -59,5 +76,6 @@ namespace Docker.DotNet.BasicAuth
                 Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
             }
         }
+#endif
     }
 }
