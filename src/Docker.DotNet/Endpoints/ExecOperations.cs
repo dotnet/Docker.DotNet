89d415c4bc18508baf43f32fdc2bf2ff97079085
Gabriel Lucaci <gabriel.lucaci.dev@gmail.com>
2019-01-30 19:33:05 +0100

    Allow ExecOperations (#366)

--- src/Docker.DotNet/Endpoints/ExecOperations.cs
+++ src/Docker.DotNet/Endpoints/ExecOperations.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Docker.DotNet.Endpoints
+namespace Docker.DotNet
 {
     internal class ExecOperations : IExecOperations
     {
