9e37f67b69e4409b3db24b2cf9bbc7931715762d
timt <timt@spgcontrols.com>
2020-10-15 15:48:58 +1100

    Added MultiplexedStreamReader

---
+++ src/Docker.DotNet/MultiplexedStreamReader.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Docker.DotNet
+{
+    public class MultiplexedStreamReader
+    {
+        private readonly MultiplexedStream _stream;
+
+        public MultiplexedStreamReader(MultiplexedStream stream)
+        {
+            _stream = stream;
+        }
+
+        public async Task<string> ReadLineAsync(CancellationToken token)
+        {
+            var line = new List<byte>();
+
+            var buffer = new byte[1];
+
+            while (true)
+            {
+                var res = await _stream.ReadOutputAsync(buffer, 0, 1, token);
+
+                if (res.Count == 0)
+                {
+                    continue;
+                }
+
+                else if (buffer[0] == '\n')
+                {
+                    return Encoding.UTF8.GetString(line.ToArray());
+                }
+
+                else
+                {
+                    line.Add(buffer[0]);
+                }
+            }
+        }
+    }
+}
