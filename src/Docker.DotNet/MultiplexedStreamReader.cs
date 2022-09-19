da4a4f0063ef95487c65b4f0c18156fab743a5a1
ACoderLife <37108462+ACoderLife@users.noreply.github.com>
2022-09-19 11:26:55 +1000

    support cancellation

--- src/Docker.DotNet/MultiplexedStreamReader.cs
+++ src/Docker.DotNet/MultiplexedStreamReader.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Docker.DotNet;
 
 namespace Docker.DotNet
 {
@@ -14,24 +15,25 @@ namespace Docker.DotNet
             _stream = stream;
         }
 
-        public async Task<string> ReadLineAsync(CancellationToken token = default)
+        public async Task<string> ReadLineAsync(CancellationToken cancellationToken)
         {
+
             var line = new List<byte>();
 
             var buffer = new byte[1];
 
-            while (true)
+            while (!cancellationToken.IsCancellationRequested)
             {
-                var res = await _stream.ReadOutputAsync(buffer, 0, 1, token);
+                var res = await _stream.ReadOutputAsync(buffer, 0, 1, cancellationToken);
 
                 if (res.Count == 0)
                 {
-                    continue;
+                    return null;
                 }
 
                 else if (buffer[0] == '\n')
                 {
-                    return Encoding.UTF8.GetString(line.ToArray());
+                    break;
                 }
 
                 else
@@ -39,6 +41,8 @@ namespace Docker.DotNet
                     line.Add(buffer[0]);
                 }
             }
+
+            return Encoding.UTF8.GetString(line.ToArray());
         }
     }
 }
